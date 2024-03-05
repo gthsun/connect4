eeac6466ad0e13425e67b91ce91d68bd7a0cb482
gthsun <sundaram.geethan@gmail.com>
2024-03-04 22:48:29 -0500

    Implemented QoL changes

--- connect4.js
+++ connect4.js
@@ -5,6 +5,7 @@ let curPlayer = player1;
 let gameOver = false;
 let board;
 let colLowest;
+let playerColor = "lightcoral";
 
 let rows = 6;
 let cols = 7;
@@ -15,7 +16,7 @@ window.onload = function() {
 
 function setGame() {
     board = [];
-    colLowest = [5, 5, 5, 5, 5, 5, 5]
+    colLowest = [5, 5, 5, 5, 5, 5, 5];
 
     for (let r = 0; r < rows; r++){
         let row = [];
@@ -27,6 +28,8 @@ function setGame() {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
+            tile.addEventListener("mouseover", highlightSpot);
+            tile.addEventListener("mouseout", unhighlightSpot);
             tile.addEventListener("click", setPiece);
             document.getElementById("board").append(tile);
         }
@@ -35,6 +38,59 @@ function setGame() {
     let gamestate = document.getElementById("gamestate");
     document.getElementById("gamestate").style.color = "blue";
     gamestate.innerText = "Click on the column you wish to place your disk at!";
+}
+
+function highlightSpot() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-"); //Splits tile ID into an array of the row and column
+    let c = parseInt(coords[1]);
+
+    r = colLowest[c]; //Sets row to be lowest available row
+
+    if(r < 0) {
+        return;
+    }
+    
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if(board[r][c] !== ' ') {
+        return;
+    } else{
+        tile.style.backgroundColor = playerColor;
+        for (let i = 0; i < r; i++) {
+            let otherTile = document.getElementById(i.toString() + "-" + c.toString());
+            otherTile.style.backgroundColor = "lightgray"
+        }
+    }
+}
+
+function unhighlightSpot() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-"); //Splits tile ID into an array of the row and column
+    let c = parseInt(coords[1]);
+
+    r = colLowest[c]; //Sets row to be lowest available row
+
+    if(r < 0) {
+        return;
+    }
+    
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+    if(board[r][c] !== ' ') {
+        return;
+    } else{
+        tile.style.backgroundColor = "white";
+        for (let i = 0; i < r; i++) {
+            let otherTile = document.getElementById(i.toString() + "-" + c.toString());
+            otherTile.style.backgroundColor = "white"
+        }
+    }
+
 }
 
 function setPiece() {
@@ -52,21 +108,27 @@ function setPiece() {
         return;
     }
 
-    colLowest[c]-= 1; //update arrays
-
     board[r][c] = curPlayer;
     let tile = document.getElementById(r.toString() + "-" + c.toString());
     if (curPlayer === player1) {
-        tile.classList.add("red-piece")
+        tile.style.backgroundColor = 'red';
         curPlayer = player2;
         gamestate.innerText = "Player 2's Turn";
         document.getElementById("gamestate").style.color = "darkgoldenrod";
+        playerColor = "rgb(251,231,161)";
     } else {
-        tile.classList.add("yellow-piece")
+        tile.style.backgroundColor = 'yellow';
         curPlayer = player1;
         gamestate.innerText = "Player 1's Turn";
         document.getElementById("gamestate").style.color = "darkred";
+        playerColor = "lightcoral";
     }
+    r-=1;
+    if(r >= 0) {
+        let aboveTile = document.getElementById(r.toString() + "-" + c.toString());
+        aboveTile.style.backgroundColor = playerColor;
+    }
+    colLowest[c]-= 1; //update arrays
 
     checkWinner();
 }
@@ -76,7 +138,7 @@ function checkWinner() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols - 3; c++) {
             if(board[r][c] !== ' ' && board[r][c] === board[r][c+1] && board[r][c] === board[r][c+2] && board[r][c] === board[r][c+3]) {
-                setWinner(r, c);
+                setWinner(r, c, "h");
                 return;
             }
         }
@@ -85,7 +147,7 @@ function checkWinner() {
     for (let c = 0; c < cols; c++) {
         for (let r = 0; r < rows - 3; r++) {
             if(board[r][c] !== ' ' && board[r][c] === board[r+1][c] && board[r][c] === board[r+2][c] && board[r][c] === board[r+3][c]) {
-                setWinner(r, c);
+                setWinner(r, c, "v");
                 return;
             }
         }
@@ -94,7 +156,7 @@ function checkWinner() {
     for (let r = 0; r < rows - 3; r++) {
         for (let c = 0; c < cols - 3; c++) {
             if(board[r][c] !== ' ' && board[r][c] === board[r+1][c+1] && board[r][c] === board[r+2][c+2] && board[r][c] === board[r+3][c+3]) {
-                setWinner(r, c);
+                setWinner(r, c, "d");
                 return;
             }
         }
@@ -103,7 +165,7 @@ function checkWinner() {
     for (let r = 3; r < rows; r++) {
         for (let c = 0; c < cols - 3; c++) {
             if(board[r][c] !== ' ' && board[r][c] === board[r-1][c+1] && board[r][c] === board[r-2][c+2] && board[r][c] === board[r-3][c+3]) {
-                setWinner(r, c);
+                setWinner(r, c, "a");
                 return;
             }
         }
@@ -112,7 +174,7 @@ function checkWinner() {
 
 }
 
-function setWinner(r, c) {
+function setWinner(r, c, type) {
     if (board[r][c] === player1) {
         gamestate.innerText = "Player 1 Wins!"
         document.getElementById("gamestate").style.color = "red";
@@ -122,6 +184,25 @@ function setWinner(r, c) {
     }
     document.getElementById("gamestate").style.scale = 1.5;
 
-    gameOver = true;
+    document.getElementById(r.toString() + "-" + c.toString()).style.border = "5px solid lime";
+    
+    if(type === 'h') {
+        document.getElementById(r.toString() + "-" + (c+1).toString()).style.border = "5px solid lime";
+        document.getElementById(r.toString() + "-" + (c+2).toString()).style.border = "5px solid lime";
+        document.getElementById(r.toString() + "-" + (c+3).toString()).style.border = "5px solid lime";
+    } else if(type === 'v') {
+        document.getElementById((r+1).toString() + "-" + c.toString()).style.border = "5px solid lime";
+        document.getElementById((r+2).toString() + "-" + c.toString()).style.border = "5px solid lime";
+        document.getElementById((r+3).toString() + "-" + c.toString()).style.border = "5px solid lime";
+    } else if(type === 'd') {
+        document.getElementById((r+1).toString() + "-" + (c+1).toString()).style.border = "5px solid lime";
+        document.getElementById((r+2).toString() + "-" + (c+2).toString()).style.border = "5px solid lime";
+        document.getElementById((r+3).toString() + "-" + (c+3).toString()).style.border = "5px solid lime";
+    } else {
+        document.getElementById((r-1).toString() + "-" + (c+1).toString()).style.border = "5px solid lime";
+        document.getElementById((r-2).toString() + "-" + (c+2).toString()).style.border = "5px solid lime";
+        document.getElementById((r-3).toString() + "-" + (c+3).toString()).style.border = "5px solid lime";
+    }
 
+    gameOver = true;
 }
